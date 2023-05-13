408ddf39390037c191156504aa00dbeb85492ea0
Cyb3R <niklasm.cyber@gmail.com>
2023-05-13 16:23:33 +0200

    all projects as class lib

--- IndustrialParkRandomizer/Program.cs
+++ IndustrialParkRandomizer/Program.cs
@@ -5,24 +5,24 @@ using System.Windows.Forms;
 
 namespace IndustrialParkRandomizer
 {
-    static class Program
-    {
-        /// <summary>
-        /// Ponto de entrada principal para o aplicativo.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-us");
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            new IndustrialPark.SharpRenderer();
-
-            RandomizerMenu menu = new RandomizerMenu();
-
-            Application.Run(menu);
-        }
-    }
+    // static class Program
+    // {
+    //     // /// <summary>
+    //     // /// Ponto de entrada principal para o aplicativo.
+    //     // /// </summary>
+    //     // [STAThread]
+    //     // static void Main()
+    //     // {
+    //     //     System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-us");
+    //     //
+    //     //     Application.EnableVisualStyles();
+    //     //     Application.SetCompatibleTextRenderingDefault(false);
+    //     //
+    //     //     new IndustrialPark.SharpRenderer();
+    //     //
+    //     //     RandomizerMenu menu = new RandomizerMenu();
+    //     //
+    //     //     Application.Run(menu);
+    //     // }
+    // }
 }
